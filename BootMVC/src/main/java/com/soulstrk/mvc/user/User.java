bd1dd4fecb450273855d7cbdcbe06bf77f99b729
soulstrk <soulstrk1234@gmail.com>
2018-12-01 17:26:38 +0900

    Spring Boot Web MVC

---
+++ BootMVC/src/main/java/com/soulstrk/mvc/user/User.java
@@ -0,0 +1,42 @@
+package com.soulstrk.mvc.user;
+
+public class User {
+	private Long id;
+	private String username;
+	private String password;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
+}

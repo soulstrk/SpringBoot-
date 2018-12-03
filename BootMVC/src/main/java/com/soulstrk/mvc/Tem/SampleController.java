a92a937d7e054c3a635ebc4f6d6ba95546f4346a
soulstrk <soulstrk1234@gmail.com>
2018-12-03 22:36:12 +0900

    훌륭한 REST API를 위한 HATEOAS 추가

--- BootMVC/src/main/java/com/soulstrk/mvc/Tem/SampleController.java
+++ BootMVC/src/main/java/com/soulstrk/mvc/Tem/SampleController.java
@@ -1,8 +1,14 @@
 package com.soulstrk.mvc.Tem;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.soulstrk.mvc.user.Hello;
 
 @Controller
 public class SampleController {
@@ -11,6 +17,19 @@ public class SampleController {
 	public String hello(Model model) {
 		model.addAttribute("name","keesun");
 		return "hello";
+	}
+	
+	@RequestMapping("/restHello")
+	public @ResponseBody Resource<Hello> restHello() {
+		Hello hello = new Hello();
+		hello.setName("keesun");
+		hello.setPrefix("lee");
+		
+		//링크 정보를 추가하는 방법
+		Resource<Hello> helloResource = new Resource<Hello>(hello);
+		helloResource.add(ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(SampleController.class).restHello()).withSelfRel());
+		
+		return helloResource;
 	}
 	
 }

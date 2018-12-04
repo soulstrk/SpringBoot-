19b40f731fcaf1449dd6ade45a3b53813753c836
soulstrk <soulstrk1234@gmail.com>
2018-12-04 11:19:05 +0900

    스프링 부트로 DB연동
    H2, HIKARICP, dataSource, jdbcTemplate

---
+++ BootMVC/src/main/java/com/soulstrk/mvc/H2Runner.java
@@ -0,0 +1,39 @@
+package com.soulstrk.mvc;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class H2Runner implements ApplicationRunner{
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		try(Connection connection = dataSource.getConnection()){
+		System.out.println(connection.getMetaData().getURL());
+		System.out.println(connection.getMetaData().getUserName());
+		
+		Statement statement = connection.createStatement();
+		String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+		// statement.execute(sql);
+		}
+		
+		// 부트에서 제공하는 jdbctemplate를 이용해서 간결하게 사용가능.
+		jdbcTemplate.execute("insert into user values(1,'keesun')");
+		
+	}
+	
+}
